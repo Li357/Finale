3faa82b2339e27ce6196ed2f338c3d1b9ac58a5e
Andrew Li <andrewli357@gmail.com>
2019-11-04 23:30:42 -0600

    React frontend generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
+
+  def frontend
+    render file: "public/index.html", layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
+  get "*path", to: "application#frontend", constraints: ->(request) { frontend_request?(request) }
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   mount ActionCable.server, at: '/cable'
+
+  def frontend_request?(request)
+    !request.xhr? && request.format.html?
+  end
 end
